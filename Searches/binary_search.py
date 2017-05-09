c4acf2ae754a67be1a1cdd6f2e744d6c889e387a
James Mc Dermott <james.mcdermott89@gmail.com>
2017-05-09 16:55:28 +0100

    Created binary_search.py with timer

---
+++ Searches/binary_search.py
@@ -0,0 +1,29 @@
+import timeit
+
+
+def bsearch(array, item):
+    low = 0
+    high = len(array)
+    while low < high:
+        mid = (low + high) // 2
+        assert low <= mid < high
+
+        if array[mid] < item:
+            low = mid + 1
+            assert low == 0 or array[low - 1] < item
+        else:
+            high = mid
+            assert item <= array[high]
+    return a[low]
+
+
+def main():
+    start_time = timeit.default_timer()
+    values = [1, 10, 45]
+    result = bsearch(values, 45)
+    elapsed = (timeit.default_timer() - start_time)
+    print(result, "was found")
+    print("Took %s seconds" % (elapsed))
+
+if '__main__' == __name__:
+    main()
