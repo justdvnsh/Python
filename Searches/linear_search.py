b12bf6166148e3ef46ba81ed05153e894a620d09
James Mc Dermott <james.mcdermott89@gmail.com>
2017-05-09 17:07:09 +0100

    changed result format

--- Searches/linear_search.py
+++ Searches/linear_search.py
@@ -5,7 +5,7 @@ def linear_search(array, item):
     for i in array:
         if i == int(item):
             return item
-    return "No item", item, "found"
+    return "No item ", item, " found"
 
 
 def main():
@@ -13,8 +13,11 @@ def main():
     array_of_values = [1, 10, 45, 26, 98, 45, 5, 22, 28, 7, 69, 0, 43, 31, 18]
     result = linear_search(array_of_values, 10)
     elapsed = (timeit.default_timer() - start_time)
-    print(result, "was found")
-    print("Took %s seconds" % (elapsed))
+    if result == str(result):
+        print(result)
+    else:
+        print(result, "was found")
 
+    print("Took %s seconds" % (elapsed))
 if '__main__' == __name__:
     main()
